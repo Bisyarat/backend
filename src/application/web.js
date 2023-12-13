495a49456ef3e87fabca14965c346a5c4f6c5845
windwalk77 <rizkya7754@gmail.com>
2023-12-14 03:51:42 +0700

    cors enabled

--- src/application/web.js
+++ src/application/web.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { publicRouter } from "../route/public-router.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../route/router.js";
@@ -6,6 +7,7 @@ import { userRouter } from "../route/router.js";
 export const web = express();
 
 web.use(express.json());
+web.use(cors())
 web.use(publicRouter);
 web.use(userRouter);
 web.use(errorMiddleware);
